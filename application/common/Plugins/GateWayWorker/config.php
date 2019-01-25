761b60ecd1f40cd6d02e783fd000accabfe9b513
vilson <545522390@qq.com>
2019-01-25 17:03:06 +0800

    更新版本号至2.1.0
    Signed-off-by: vilson <545522390@qq.com>

--- application/common/Plugins/GateWayWorker/config.php
+++ application/common/Plugins/GateWayWorker/config.php
@@ -1,10 +1,10 @@
 <?php
-define('SERVER_ADDRESS', '172.18.176.189');//服务注册地址，需为内网IP地址
+define('SERVER_ADDRESS', '192.168.0.159');//服务注册地址，需为内网IP地址
 define('SERVER_PORT', '2346');//服务注册端口
 define('CLIENT_PORT', '2345');//客户端监听端口
 
 //ssl配置 请使用绝对路径。不开启可以不用关注
-define('USE_SSL', true);//是否使用ssl
+define('USE_SSL', false);//是否使用ssl
 define('LOCAL_CERT', '/www/wwwroot/EasyProjectApi/server.pem');// 证书路径也可以是crt文件
 define('LOCAL_PK', '/www/wwwroot/EasyProjectApi/server.key');
 define('VERIFY_PEER', false);
