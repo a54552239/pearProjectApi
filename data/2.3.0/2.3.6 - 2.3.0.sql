b614f738697c290a9c0fd6d84bee1a9dcb3371f2
vilson <545522390@qq.com>
2019-06-16 06:06:49 +0800

    更新数据库文件
    Signed-off-by: vilson <545522390@qq.com>

--- data/2.3.0/2.3.6 - 2.3.0.sql
+++ data/2.3.0/2.3.6 - 2.3.0.sql
@@ -4,6 +4,8 @@ ALTER TABLE `pearproject`.`pear_project` ADD COLUMN `open_begin_time` tinyint(1)
 
 ALTER TABLE `pearproject`.`pear_project` ADD COLUMN `open_task_private` tinyint(1) NULL DEFAULT 0 COMMENT '是否开启新任务默认开启隐私模式' AFTER `open_begin_time`;
 
+ALTER TABLE `pearProject`.`pear_project` ADD COLUMN `task_board_theme` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT 'default' COMMENT '看板风格' AFTER `open_task_private`;
+
 ALTER TABLE `pearproject`.`pear_task` ADD COLUMN `schedule` int(3) NULL DEFAULT 0 COMMENT '进度百分比' AFTER `path`;
 
 SET FOREIGN_KEY_CHECKS=1;
