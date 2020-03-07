d3943d1f325548e4576748414df7fa7a437edd5e
vilson <545522390@qq.com>
2020-03-07 10:22:45 +0800

    更新数据库文件
    Signed-off-by: vilson <545522390@qq.com>

--- data/2.8.0/2.8.13-2.8.14.sql
+++ data/2.8.0/2.8.13-2.8.14.sql
@@ -1,6 +1,6 @@
 SET FOREIGN_KEY_CHECKS=0;
 
-INSERT INTO `pearProject`.`pear_system_config`(`id`, `name`, `value`) VALUES (44, 'single_mode', '0');
-INSERT INTO `pearProject`.`pear_system_config`(`id`, `name`, `value`) VALUES (45, 'single_org_code', '');
+INSERT INTO `pearProject`.`pear_system_config`(`name`, `value`) VALUES ('single_mode', '0');
+INSERT INTO `pearProject`.`pear_system_config`(`name`, `value`) VALUES ('single_org_code', '');
 
 SET FOREIGN_KEY_CHECKS=1;
