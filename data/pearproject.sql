73c394ebe6e8704e78951532dbfde632f5847e8c
vilson <545522390@qq.com>
2022-05-22 10:49:25 +0800

    细节调整

--- data/pearproject.sql
+++ data/pearproject.sql
@@ -5,7 +5,7 @@
  Source Server Type    : MySQL
  Source Server Version : 50726
  Source Host           : localhost:3306
- Source Schema         : pearprojectpro
+ Source Schema         : pearproject
 
  Target Server Type    : MySQL
  Target Server Version : 50726
